e0080f17ca4923f231c75921bdbe35765891327c
Swapnil Dhamal <51965205+swapnil7895@users.noreply.github.com>
2021-02-25 19:13:47 +0530

    updated with new functionalities and some validations.

--- Voting.sol
+++ Voting.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.8.0; 
 
 contract Voting
@@ -14,7 +13,10 @@ contract Voting
     uint public maxVotes;
     uint prevMaxVotes;
     bool isResultCalculated;
-
+    bool votingCanBeStartOnlyOnce;
+    uint public howMnayVoted;
+    //string  winnerName;
+    
     //arrays
     //Candidate candidatesArray[]; // for storing candidates.
     address [] voted;
@@ -51,6 +53,8 @@ contract Voting
 
     function startVoting()public isOwner
     {
+        require (votingCanBeStartOnlyOnce==false,"Voting can be done only once");
+        require (isResultCalculated==false,"Voting can be started only once");
         require ( votingStarted==false,"Voting is already in progress");
         votingStarted=true;
     }   
@@ -70,6 +74,7 @@ contract Voting
     function giveAccessForCandidature(uint _id) public isOwner
     {
         //only organizer can call
+        require (votingCanBeStartOnlyOnce==false,"Voting is over, no use of giving access.");
         require( votingStarted == true ,"Voting is not started yet, Please wait for it, Then apply");
         candidates[_id].canFight = true;
     }
@@ -77,10 +82,12 @@ contract Voting
     function vote( uint _id )public
     {
         //anyone can vote   //but one person can vote only once
+        require (votingCanBeStartOnlyOnce==false,"Cant vote now, Voting is over");
         require ( votedOrNot[msg.sender] == false, "You are already voted" );
         require ( candidates[_id].canFight == true, "This candidate is not approved yet to fight election" );
         candidates[_id].TotalVotes++;
         votedOrNot[msg.sender]=true;
+        howMnayVoted++;
     }
 
     //array for total votes of voters
@@ -88,8 +95,14 @@ contract Voting
     function calculateResult( ) public isOwner
     {
         //only owner can call this function
+        if(candidateCounter>1)
+        {
+            require ( howMnayVoted!=0,"Nobody voted, cant calculate result.");
+            
+        }
+        require (isResultCalculated==false,"Voting calculation is done only once");
         require ( votingStarted == false ,"Voting is still on,first end it.");
-        require ( candidateCounter!=0,"Voting did not happened, Start voting and then check results.");
+        require ( candidateCounter!=0,"There is no candidate fighting election, Please add them first.");
         for ( i=1; i<=candidateCounter;i++)
             {
               if(maxVotes>prevMaxVotes || maxVotes==0 )
@@ -105,18 +118,26 @@ contract Voting
     function seeWinner( )public view returns(string memory) 
     {
         //anyone can call
+        
         require( isResultCalculated ==true, "Result is not calculated yet.");
         require ( votingStarted==false,"Voting is not done yet");
         //require ( maxVoterId !=0 ," Result is not calculated yet " );
         string memory winnerName=candidates[maxVotersId].name;
-        return winnerName;
+        if(candidateCounter==1)
+        {
+            winnerName=candidates[1].name;
+            return winnerName;
+        }else
+        {return winnerName;}
     }
   
     function endVoting()public isOwner
     {
+       
         require ( votingStarted==true,"Voting is not started or already ended");
         //only organizer can call
         votingStarted=false;
+        votingCanBeStartOnlyOnce=true;
     }
 
     function isVotingIsOn()public view returns(bool)// to see whether voting is on or not
