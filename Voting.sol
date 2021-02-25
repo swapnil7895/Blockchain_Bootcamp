390f8cbc8678d3a16dfebe10bfe1cec615f8fab8
Swapnil Dhamal <51965205+swapnil7895@users.noreply.github.com>
2021-02-25 19:52:56 +0530

    cleaned code and added more comments to understand code.

--- Voting.sol
+++ Voting.sol
@@ -4,26 +4,21 @@ contract Voting
 {
 
     //state variables
-    bool votingStarted; //for start/end voting.
-    address owner; //for storing organzizer's address.
-    uint maxID;
-    uint public candidateCounter;//for knowing how many candidates are there
-    uint i;//for iteration of seeing total votes of candidates
-    uint maxVotersId;// winnner
-    uint public maxVotes;
-    uint prevMaxVotes;
-    bool isResultCalculated;
-    bool votingCanBeStartOnlyOnce;
-    uint public howMnayVoted;
-    //string  winnerName;
+    bool votingStarted;                                                     //for start/end voting.
+    address owner;                                                          //for storing organzizer's address.
+    uint public candidateCounter;                                           //for getting how many candidates are there
+    uint i;                                                                 //for iteration of seeing total votes of candidates
+    uint maxVotersId;                                                       //for storing winner's id.
+    uint public maxVotes;                                                   // winnner votes
+    uint prevMaxVotes;                                                      //used in loop
+    bool isResultCalculated;                                                //for restricting voting to only once.
+    bool votingCanBeStartOnlyOnce;                                          //for restricting voting to only once.
+    uint public howManyVoted;                                               //total voters
     
-    //arrays
-    //Candidate candidatesArray[]; // for storing candidates.
-    address [] voted;
-
+    
     constructor()
     {
-        owner=msg.sender;
+        owner=msg.sender;                                                   //assigning deployer as owner.
     }
     
     struct Candidate
@@ -36,7 +31,8 @@ contract Voting
 
 
     // modifiers
-    //1 isOwner
+    //1. isOwner
+    
     modifier isOwner()
     {
         require(msg.sender==owner,"only owner can call this");
@@ -46,101 +42,90 @@ contract Voting
 
     //mappings
     //1
-    mapping ( uint => Candidate) candidates;
+    mapping ( uint => Candidate) candidates;                                        //mapping for access candidate with ID.
+    
     //2 for voted or not  
-    mapping ( address => bool ) votedOrNot;
+    mapping ( address => bool ) votedOrNot;                                         //mapping for change voter's state-non voted to voted.
 
 
-    function startVoting()public isOwner
+    function startVoting () public isOwner
     {
-        require (votingCanBeStartOnlyOnce==false,"Voting can be done only once");
-        require (isResultCalculated==false,"Voting can be started only once");
-        require ( votingStarted==false,"Voting is already in progress");
+        require (votingCanBeStartOnlyOnce==false,"Voting can be done only once");   //To restrict voting start more than once.
+        require (isResultCalculated==false,"Voting can be started only once");      //To restrict voting start more than once.
+        require ( votingStarted==false,"Voting is already in progress");            //if treid to start when it's already started
         votingStarted=true;
     }   
 
-    function applyForCandidature(string memory _name, uint _age)public 
+    function applyForCandidature (string memory _name, uint _age) public 
     {   
-        //organizer cant be candidate
-        require(msg.sender!=owner, "Owner cant participate in election");
-        require(votingStarted==true,"Voting is not started yet, Please wait for it, Then apply");
-        Candidate memory candidate = Candidate (_name, _age, 0, false );
-        candidateCounter++;
-        candidates [ candidateCounter ] = candidate; //passing newly created Candidate instance to mapping
-        //candidatesArray[CandidateCounter]=candidate;// no need i'll remove this later.
-        
-
+        require(msg.sender!=owner, "Owner cant participate in election");           //owner cant participate.
+        require(votingStarted==true,"Voting is not started yet, Please wait for it, Then apply");  //voting must be started for applying.
+        Candidate memory candidate = Candidate (_name, _age, 0, false );            //creating instance of candidate.
+        candidateCounter++;                                                         //how many candidates are here till now.
+        candidates [ candidateCounter ] = candidate;                                //passing newly created Candidate instance to mapping.
     }
-    function giveAccessForCandidature(uint _id) public isOwner
-    {
-        //only organizer can call
-        require (votingCanBeStartOnlyOnce==false,"Voting is over, no use of giving access.");
-        require( votingStarted == true ,"Voting is not started yet, Please wait for it, Then apply");
-        candidates[_id].canFight = true;
-    }
-
-    function vote( uint _id )public
-    {
-        //anyone can vote   //but one person can vote only once
-        require (votingCanBeStartOnlyOnce==false,"Cant vote now, Voting is over");
-        require ( votedOrNot[msg.sender] == false, "You are already voted" );
-        require ( candidates[_id].canFight == true, "This candidate is not approved yet to fight election" );
-        candidates[_id].TotalVotes++;
-        votedOrNot[msg.sender]=true;
-        howMnayVoted++;
-    }
-
-    //array for total votes of voters
     
-    function calculateResult( ) public isOwner
+    function giveAccessForCandidature (uint _id) public isOwner
     {
-        //only owner can call this function
-        if(candidateCounter>1)
+        require ( votingCanBeStartOnlyOnce==false,"Voting is over, no use of giving access.");
+        require( votingStarted == true ,"Voting is not started yet, Please wait for it, Then apply");
+        candidates[_id].canFight = true;                                            //giving access to candidate for fight election.
+    }
+
+    function vote ( uint _id ) public
+    {
+        require (votingCanBeStartOnlyOnce==false,"Cant vote now, Voting is over");
+        require ( votedOrNot[msg.sender] == false, "You are already voted" );       //checking if voter  voded or not
+        require ( candidates[_id].canFight == true, "This candidate is not approved yet to fight election" );   //checking if candidate can fight or not.
+        candidates[_id].TotalVotes++;                                               //incrementing votes of candidate.
+        votedOrNot[msg.sender]=true;                                                //marking voter as voted.
+        howMnayVoted++;                                                             //counting total votes.
+    }
+    
+    function calculateResult ( ) public isOwner
+    {
+        if(candidateCounter>1)                                                      //if there is only one candidate in election then he is automatically elected, whether anybody voted or not.
         {
-            require ( howMnayVoted!=0,"Nobody voted, cant calculate result.");
-            
+            require ( howManyVoted!=0,"Nobody voted, can't calculate result.");     //if there is more than one candidate then there will be checking of votes.
         }
-        require (isResultCalculated==false,"Voting calculation is done only once");
+        
+        require (isResultCalculated==false,"Voting calculation is done only once"); 
         require ( votingStarted == false ,"Voting is still on,first end it.");
-        require ( candidateCounter!=0,"There is no candidate fighting election, Please add them first.");
-        for ( i=1; i<=candidateCounter;i++)
+        require ( candidateCounter!=0,"There is no candidate fighting election, Please add them first.");  //if there is no candidate.
+        for ( i=1; i<=candidateCounter;i++)                                          //for finding winner. 
             {
-              if(maxVotes>prevMaxVotes || maxVotes==0 )
+              if(maxVotes>prevMaxVotes || maxVotes==0 )                             
               {
                 maxVotes=candidates[i].TotalVotes;
                 maxVotersId=i;
               }
              prevMaxVotes=maxVotes;
             }
-        isResultCalculated=true;
+        isResultCalculated=true;                                                     //flag for calculating result only once.
     }
 
-    function seeWinner( )public view returns(string memory) 
-    {
-        //anyone can call
-        
+    function seeWinner ( ) public view returns (string memory) 
+    {   
         require( isResultCalculated ==true, "Result is not calculated yet.");
         require ( votingStarted==false,"Voting is not done yet");
-        //require ( maxVoterId !=0 ," Result is not calculated yet " );
-        string memory winnerName=candidates[maxVotersId].name;
-        if(candidateCounter==1)
+        string memory winnerName=candidates[maxVotersId].name;                       //assigning max voter as winner.
+        if(candidateCounter==1)                                                      //if there is only one candidate and no voter-so that to select that candiidate as winner.
         {
             winnerName=candidates[1].name;
             return winnerName;
         }else
-        {return winnerName;}
+        {return winnerName;}                                                        //return winner name.
     }
   
     function endVoting()public isOwner
     {
        
-        require ( votingStarted==true,"Voting is not started or already ended");
-        //only organizer can call
-        votingStarted=false;
-        votingCanBeStartOnlyOnce=true;
+        require ( votingStarted==true, "Voting is not started or already ended");
+        votingStarted=false;                                                        //to end voting
+        votingCanBeStartOnlyOnce=true;                                              //to ensure voting can only be done once.
     }
 
-    function isVotingIsOn()public view returns(bool)// to see whether voting is on or not
+    function isVotingIsOn () public view returns (bool)                             // to see whether voting is on or not
     {
         return votingStarted;
     }
